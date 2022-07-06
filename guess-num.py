0e2bdeb88a53db816e60f8258ba9491876712c25
Anne Chiang <anneannechiang@gmail.com>
2022-07-06 16:55:18 +0800

    basic version

---
+++ guess-num.py
@@ -0,0 +1,23 @@
+#產生一個隨機整數（1-100) 
+#讓使用者重覆輸入數字去猜
+#猜對, 印出【猜對了！】
+#猜錯, 要提示, 比答案大/小
+
+import random
+
+r = random.randint(1, 100)
+while True:
+	num = input('Guess a number from 1 to 100.. ')
+	num = int(num)
+	if r == num:
+		print('Great Job! You are correct! ')
+		break
+		
+	elif r > num:
+		print('The number is higher than ', num, '!')
+	else:
+		print('The number is lower than ', num, '!')
+
+
+
+
