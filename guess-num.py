a62bf06f9f4ffd90145cc3b55c58e6933dedc189
Anne Chiang <anneannechiang@gmail.com>
2022-07-06 17:24:33 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -1,15 +1,19 @@
-#產生一個隨機整數（1-100) 
+#使用者設定數字範圍, 系統隨機出 
 #讓使用者重覆輸入數字去猜
 #猜對, 印出【猜對了！】
 #猜錯, 要提示, 比答案大/小
 
 import random
+start = input('What is your minimum number ? ')
+end = input('What is your maximum number? ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 4)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1
-	num = input('Guess a number from 1 to 100.. ')
+	num = input('Guess a number now.. ')
 	num = int(num)
 	if r == num:
 		print('Great Job! You are correct. ')
@@ -17,9 +21,9 @@ while True:
 		break
 		
 	elif r > num:
-		print('The number is higher than ', num, '!')
+		print('The number is higher than', num, '!')
 	else:
-		print('The number is lower than ', num, '!')
+		print('The number is lower than', num, '!')
 	print('You have guessed', count, 'times..')
 
 
